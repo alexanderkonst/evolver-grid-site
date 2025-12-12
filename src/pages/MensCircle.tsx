b86e38a77c5689f3077b59a059a934243396c032
alexanderkonst <alexanderkonst@gmail.com>
2025-12-12 14:13:38 +0800

    Change lion photo

--- src/pages/MensCircle.tsx
+++ src/pages/MensCircle.tsx
@@ -332,7 +332,7 @@ const MensCircle = () => {
         <div className="max-w-3xl mx-auto space-y-6 md:space-y-8">
           <div className="text-center mb-2 md:mb-4">
             <img
-              src="https://i.imgur.com/eyruvkFm.png"
+              src="https://i.imgur.com/EEe29pz.jpeg"
               alt="For Whom"
               className="w-20 h-20 md:w-32 md:h-32 mx-auto object-cover rounded-full"
               style={{ objectPosition: '40% center' }}
