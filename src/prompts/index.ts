e1e431fc382a21571d2d0d529c6c16b355f40ecf
alexanderkonst <alexanderkonst@gmail.com>
2026-06-06 15:26:41 -0600

    deploy

--- src/prompts/index.ts
+++ src/prompts/index.ts
@@ -1,5 +1,6 @@
 export { ASSET_MAPPING_PROMPT } from "./extraction/assetMappingPrompt";
 export { GENIUS_OFFER_PROMPT_FULL, GENIUS_OFFER_PROMPT_SHORT } from "./user/geniusOfferPrompts";
+export { ITERATE_STRATEGY_CRUCIBLE_V2_PROMPT } from "./user/iterateStrategyCruciblePrompt";
 export { MISSION_DISCOVERY_PROMPT } from "./user/missionDiscoveryPrompt";
 export { MATCH_WHY_PROMPT, extractConnectionHook, stripConnectionHook } from "./user/matchWhyPrompt";
 export { ZONE_OF_GENIUS_PROMPT } from "./user/zoneOfGeniusPrompt";
